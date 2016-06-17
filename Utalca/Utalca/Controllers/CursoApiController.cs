10012d671905cd55e55988e40186ca982fb0be16
kvergara10 <kvergara10@alumnos.utalca.cl>
2016-06-17 15:52:35 -0400

    ProyectoTecWeb

---
+++ Utalca/Utalca/Controllers/CursoApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Utalca.Controllers
+{
+    public class CursoApiController : ApiController
+    {
+
+
+        // GET: api/CursoApi/ID
+        public string Get(long id)
+        {
+            return "value";
+        }
+
+        // PUT: api/
+        public void Post(long idCurso, long idClase, long idParticipante)
+        {
+            var servicio = new ControlAsistencia.ControlAsistenciaClient();
+            servicio.RegistrarAsistencia(idParticipante, idCurso, idClase);
+        }
+
+    }
+}
---
+++ Utalca/Utalca/Controllers/CursoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Utalca.Controllers
+{
+    public class CursoController : Controller
+    {
+        // GET: Curso
+        public ActionResult Index()
+        {
+            var servicio = new ControlAsistencia.ControlAsistenciaClient();
+            var cursos = servicio.Cursos();
+            return View(cursos);
+        }
+
+        public ActionResult Details(long id)
+        {
+            var servicio = new ControlAsistencia.ControlAsistenciaClient();
+            var detalleCurso = servicio.Curso(id);
+            return View(detalleCurso);
+        }
+
+        public ActionResult Horario(long id )
+        {
+            var servicio = new ControlAsistencia.ControlAsistenciaClient();
+            var curso = servicio.Curso(id);
+            return View(curso);
+        }
+
+        public ActionResult RegistroAsistencia(long id)
+        {
+            var servicio = new ControlAsistencia.ControlAsistenciaClient();
+            var curso = servicio.Curso(id);
+            return View(curso);
+        }
+
+
+    }
+}
